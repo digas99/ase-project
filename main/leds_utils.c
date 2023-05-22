701a1adda77ca9538f162e4142f9cb130c7712f3
Diogo Correia <digas_correia@hotmail.com>
2023-05-23 00:13:42 +0100

    functions to handle leds

--- main/leds_utils.c
+++ main/leds_utils.c
@@ -5,38 +5,35 @@
 #include "led_strip.h"
 #include "sdkconfig.h"
 #include "esp_log.h"
-    
-// GPIO pin number for blinking LED
-#define CONFIG_BLINK_GPIO 4
-
-#define BLINK_GPIO CONFIG_BLINK_GPIO
 
 void blink_led_task(void*);
 
-static void blink_led(uint8_t s_led_state)
+static void blink_led(uint8_t led_gpio, uint8_t s_led_state)
 {
     /* Set the GPIO level according to the state (LOW or HIGH)*/
-    gpio_set_level(BLINK_GPIO, s_led_state);
+    gpio_set_level(led_gpio, s_led_state);
 }
 
-static void configure_led(void)
+static void configure_led(uint8_t led_gpio)
 {
-    gpio_reset_pin(BLINK_GPIO);
+    gpio_reset_pin(led_gpio);
     /* Set the GPIO as a push/pull output */
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+    gpio_set_direction(led_gpio, GPIO_MODE_OUTPUT);
 }
 
 // portTICK_PERIOD_MS is the temporal resolution of the FreeRTOS tick timer
 
-void blink_led_task(void * pvParameter) {
-    configure_led();
+void blink_led_task(void* gpio_pin) {
+    uint8_t pin = (uint8_t)gpio_pin; 
 
-    uint32_t freq = (uint32_t)pvParameter;
+    configure_led(pin);
+
+    uint32_t freq = 100;
     uint32_t period = 1000 / freq; // period in ms
 
     uint8_t s_led_state = 0;
     while (1) {
-        blink_led(s_led_state);
+        blink_led(pin, s_led_state);
         s_led_state = !s_led_state;
         vTaskDelay(period / portTICK_PERIOD_MS);
     }
--- main/rfid.c
+++ main/rfid.c
@@ -8,12 +8,55 @@
 #define PIN_SPI_MOSI 23
 #define PIN_SPI_MISO 19
 
+#define red_on() turn_on_led(2)
+#define red_off() turn_off_led(2)
+#define green_on() turn_on_led(4)
+#define green_off() turn_off_led(4)
+
+void turn_on_led(uint8_t);
+void turn_off_led(uint8_t);
+
+TaskHandle_t green_led_task_handle = NULL;
+TaskHandle_t red_led_task_handle = NULL;
+
 void app_main(void)
 {
-    // test led
-    uint16_t freq = 100;
+    uint8_t led_flag = 0;
+    while (1)
+    {
+        if (led_flag == 0) {
+            green_off();
+            vTaskDelay(100 / portTICK_PERIOD_MS);  // Delay to ensure previous LED is turned off
+            red_on();
+        } else {
+            red_off();
+            vTaskDelay(100 / portTICK_PERIOD_MS);  // Delay to ensure previous LED is turned off
+            green_on();
+        }
 
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        led_flag = !led_flag;
+    }
+    
+}
+
+void turn_on_led(uint8_t led_gpio_pin) {
     TaskHandle_t blink_led_task_handle = NULL;
-    xTaskCreate(blink_led_task, "blink_led_task", 2048, (void*)freq, 5, &blink_led_task_handle);
+
+    xTaskCreate(blink_led_task, "led_task", 2048, (void *)led_gpio_pin, 5, &blink_led_task_handle);
     configASSERT(blink_led_task_handle);
+
+    if (led_gpio_pin == 2)
+        red_led_task_handle = blink_led_task_handle;
+    
+    if (led_gpio_pin == 4)
+        green_led_task_handle = blink_led_task_handle;
+}
+
+void turn_off_led(uint8_t led_gpio_pin) {
+    if (led_gpio_pin == 2 && red_led_task_handle != NULL)
+        vTaskDelete(red_led_task_handle);
+    
+    if  (led_gpio_pin == 4 && green_led_task_handle != NULL)
+        vTaskDelete(green_led_task_handle);
 }
