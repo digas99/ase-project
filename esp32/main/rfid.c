01b7f907b8e6a05394dccd7e67ee45b4e23cb570
Diogo Correia <digas_correia@hotmail.com>
2023-06-04 23:01:30 +0100

    added eeprom module

--- esp32/main/rfid.c
+++ esp32/main/rfid.c
@@ -3,17 +3,18 @@
 #include <inttypes.h>
 
 #include "rc522.h"
+#include "spi_25LC040A_eeprom.h"
 #include "driver/gpio.h"
 #include "driver/ledc.h"
 
 #include "leds_utils.c"
 
-#define SPI_MASTER_HOST SPI3_HOST
-
-#define PIN_SPI_CS 5
 #define PIN_SPI_CLK 18
 #define PIN_SPI_MOSI 23
 #define PIN_SPI_MISO 19
+#define PIN_RC55_CS 5
+#define PIN_EEPROM_CS 16
+#define CLK_SPEED_HZ 1000000
 
 #define PIN_GREEN_LED 4
 #define PIN_RED_LED 2
@@ -28,6 +29,8 @@
 #define green_on() turn_on_led(PIN_GREEN_LED)
 #define green_off() turn_off_led(PIN_GREEN_LED)
 
+esp_err_t rc522_init(bool);
+
 void turn_on_led(uint8_t);
 void turn_off_led(uint8_t);
 
@@ -41,8 +44,13 @@ void forb_seq();
 TaskHandle_t green_led_task_handle = NULL;
 TaskHandle_t red_led_task_handle = NULL;
 
-static const char* TAG = "rc522";
+volatile bool write_test = true;
+
+static const char* RC522_TAG = "rc522";
 static rc522_handle_t scanner;
+
+static const char* EEPROM_TAG = "eeprom";
+spi_device_handle_t spi_device;
 
 static void rc522_handler(void* arg, esp_event_base_t base, int32_t event_id, void* event_data)
 {
@@ -51,15 +59,29 @@ static void rc522_handler(void* arg, esp_event_base_t base, int32_t event_id, vo
     switch(event_id) {
         case RC522_EVENT_TAG_SCANNED: {
                 rc522_tag_t* tag = (rc522_tag_t*) data->ptr;
-                ESP_LOGI(TAG, "Tag scanned (sn: %" PRIu64 ")", tag->serial_number);
+                ESP_LOGI(RC522_TAG, "Tag scanned (sn: %" PRIu64 ")", tag->serial_number);
+                
+                if (write_test) {
+                    uint8_t data[1] = {0x05};
+                    spi_25LC040_write_enable(spi_device);
+                    spi_25LC040_write_byte(spi_device, 0x00, data[0]);
+                    spi_25LC040_write_disable(spi_device);
+                    ESP_LOGI(EEPROM_TAG, "Write data: 0x%02X", data[0]);
+                }
+                else {
+                    uint8_t data[1];
+                    spi_25LC040_read_byte(spi_device, 0x00, data);
+                    ESP_LOGI(EEPROM_TAG, "Read data: 0x%02X", data[0]);
+                }
+                write_test = !write_test;
                 
                 if (request_access(tag->serial_number)) {
-                    ESP_LOGI(TAG, "Access granted");
+                    ESP_LOGI(RC522_TAG, "Access granted");
                 }
                 else {
                     forb_seq();
-                    ESP_LOGI(TAG, "Access denied");
-                }
+                    ESP_LOGI(RC522_TAG, "Access denied");
+                    }
             }
             break;
     }
@@ -69,17 +91,44 @@ void app_main(void)
 {
     ledc_buzzer_init();
 
+    spi_25LC040_init(VSPI_HOST, PIN_EEPROM_CS, PIN_SPI_CLK, PIN_SPI_MOSI, PIN_SPI_MISO, CLK_SPEED_HZ, &spi_device);
+    spi_25LC040_write_status(spi_device, 0x0); 
+
+    rc522_init(true);
+}
+
+esp_err_t rc522_init(bool attach_to_bus) {
     rc522_config_t config = {
         .spi.host = VSPI_HOST,
-        .spi.miso_gpio = PIN_SPI_MISO,
-        .spi.mosi_gpio = PIN_SPI_MOSI,
-        .spi.sck_gpio = PIN_SPI_CLK,
-        .spi.sda_gpio = PIN_SPI_CS,
+        .spi.sda_gpio = PIN_RC55_CS,
+        .spi.bus_is_initialized = attach_to_bus
     };
 
-    rc522_create(&config, &scanner);
-    rc522_register_events(scanner, RC522_EVENT_ANY, rc522_handler, NULL);
-    rc522_start(scanner);
+    if (!attach_to_bus) {
+        config.spi.sck_gpio = PIN_SPI_CLK;
+        config.spi.mosi_gpio = PIN_SPI_MOSI;
+        config.spi.miso_gpio = PIN_SPI_MISO;
+    }
+    
+    esp_err_t ret = rc522_create(&config, &scanner);
+    if (ret != ESP_OK) {
+        ESP_LOGE(RC522_TAG, "Failed to create scanner: %d", ret);
+        return ret;
+    }
+
+    ret = rc522_register_events(scanner, RC522_EVENT_ANY, rc522_handler, NULL);
+    if (ret != ESP_OK) {
+        ESP_LOGE(RC522_TAG, "Failed to register events: %d", ret);
+        return ret;
+    }
+
+    ret = rc522_start(scanner);
+    if (ret != ESP_OK) {
+        ESP_LOGE(RC522_TAG, "Failed to start scanner: %d", ret);
+        return ret;
+    }
+
+    return ret;
 }
 
 bool request_access(uint64_t serial_number) {
